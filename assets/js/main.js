1c08128bea319e0046245c558e21ab2d87346cb4
NguyenThanhHuyen <huyen.nguyen@staff.gumiviet.com>
2021-03-25 14:16:05 +0700

    fix header-banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,27 +20,20 @@ const navSlide = () => {
 
 navSlide();
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
+
+let slideIndex = 1;
+showSlides(slideIndex);
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
+
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("banner__slider");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
+    let i;
+    let slides = document.getElementsByClassName("banner__slider__item");
+    let dots = document.getElementsByClassName("dot-banner");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -49,4 +42,6 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+
+
 }
